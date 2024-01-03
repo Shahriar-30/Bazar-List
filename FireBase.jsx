2dec6bbd002888bb434448113aef08fb7cc5521e
Shahriar Amin Al Siphat <alsiphat30@gmail.com>
2024-01-03 04:16:35 -0800

    fixed the database

--- FireBase.jsx
+++ FireBase.jsx
@@ -2,14 +2,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAVQ_3c_L46HT9SAlJvHt_ZL7mBvOjJYek",
-  authDomain: "realtime-database-81411.firebaseapp.com",
-  databaseURL: "https://realtime-database-81411-default-rtdb.firebaseio.com",
-  projectId: "realtime-database-81411",
-  storageBucket: "realtime-database-81411.appspot.com",
-  messagingSenderId: "545874014982",
-  appId: "1:545874014982:web:9533296942ba841c406487",
-  databaseURL: "https://realtime-database-81411-default-rtdb.firebaseio.com/"
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXXXXXXXXXX"
 };
 
 
